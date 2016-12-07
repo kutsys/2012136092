220ab8c115915959c587c02963704e95a76cf3db
LSG-PC\LSG <lsb154@naver.com>
2016-12-07 11:25:43 +0900

    test

--- Lab3/highcopy.c
+++ Lab3/highcopy.c
@@ -1,31 +1,44 @@
-    #include <unistd.h>
-    #include <sys/stat.h>
-    #include <fcntl.h>
-    #include <stdlib.h>
-    #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+// HighLevel 파일 처리 함수를 이용한 Copy 에서는 
+//  stdio.h에 있는 표준 IO 라이브러리 함수들을 이용한다.
 
-    int main(int argc, char *argv[]) {
-        char block[1024];
-        FILE *in, *out;
-        
-        if(argc != 3) {
-            printf("오류:입력, 출력 파일의 이름을 입력하세요.\n");
-            exit(1);
-        }
+const int TIMEINTERVAL = 2;
 
-        if((in = fopen(argv[1], "r")) == NULL) {
-            printf("파일을 찾을 수 없습니다.\n");
-            exit(1);
-        }
-        out = fopen(argv[2], "w");
+int main() { // 여기서는 인자를 넣지 않고 직접 파일을 지정하였다.
+	FILE *in, *out;
+	char c[64]; // 64byte씩 받아오기
+	time_t time1, time2;
+	int temp = 0;
+	// 복사당할 파일은 test.mp4, 복사해서 생성할 파일은 test.out으로 쓰겠다.
+	if ( (in = fopen("test.mp4", "r")) == NULL) {
+		printf("Cannot open the Read file\n");
+		exit(1);
+	} // 복사할 파일을 열 때 오류가 나면 출력한다.
 
-        while(fgets(block, 1024, in)!= NULL) {
-           fputs(block,out);       
-           printf(".");
-        }
-        
-        fclose(in);
-        fclose(out);
-        printf("\ncopy end\n");
-        return 0;
-    }
+	if ( (out = fopen("file.out", "w")) == NULL ) { 
+		printf("Cannot Open  the  Write file\n");
+		exit(1);
+	} // 복사를 당할  파일을 열 때  오류가 나면 오류를 출력한다.
+	
+	time(&time1);	
+	while((temp = fread(c, sizeof(char), sizeof(c)/sizeof(char), in)) >0) {
+		time(&time2);
+		fwrite(c, sizeof(char), temp, out);
+		if ((time2 - time1) ==  1) {
+			printf("*\n");
+			sleep(1);
+			time2 = time1;
+		}
+	}	
+
+	fclose(in);
+	fclose(out);
+	//파일 닫기
+	printf("%s -> %s \n","test.mp4", "test.out");
+	printf("파일 복사 완료!!");
+
+	exit(0);
+}
--- Lab3/lowcopy.c
+++ Lab3/lowcopy.c
@@ -1,35 +1,65 @@
-    #include <unistd.h>
-    #include <sys/stat.h>
-    #include <fcntl.h>
-    #include <stdlib.h>
-    #include <stdio.h>
-    #include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+const int NAMESIZE = 50;
+const int TIMEINTERVAL = 2; // 실제로 몇 초가 지난 후에 .을 찍어내는지
+ 
+int main(int argc, char *argv[]) {
+	char fname_in[NAMESIZE],fname_out[NAMESIZE];
+	 // 각각 복사할 파일 이름, 복사당할  파일 이름
+	int in, out;
+	char buf[64]; // 64byte씩 전송을 위한 버퍼
+	time_t time1, time2;
+	
+	// 명령어의 인자가 3이 아닌경우 조건에 맞지 않음
+	// 실행파일 + in파일 + out파일 : 3개의 인자가 필수적임
+	if (argc != 3) {
+		printf("Please Write that:LowCopy [inFile] [outFile] shape\n");
+		exit(1);
+	}
 
-    int main(int argc, char *argv[]) {
-        char block[1024];
-        int in, out;
-        int read;
-        char input[30], output[30];
+	// 파일의 길이가 설정한 NAMESIZE값 보다 클 경우
+	// strlen 함수를 이용하여 파라미터로 들어온 파일이름 검사
+	if (strlen(argv[1]) > NAMESIZE || strlen(argv[2]) > NAMESIZE ) {
+		printf("Please Write filename below 50\n");
+		exit(1);
+	}
 
-        if(argc != 3) {
-            printf("오류:입력, 출력 파일의 이름을 입력하세요.\n");
-            exit(1);
-        }
+	strcpy(fname_in, argv[1]);
+	strcpy(fname_out, argv[2]);
 
-        strcpy(input, argv[1]);
-        strcpy(output, argv[2]);
+	in = open(fname_in, O_RDONLY);
+	//  in에다가 fname_in을 읽기전용으로 열고 난 파일 서술자를 반환한다.
+	// in은 fname_in 파일에 대한 파일 디스크립터다.
+	if ( in < 0 ) { 
+		printf("Cannot open the file\n");
+		exit(1);
+	} // 파일 읽기에 오류가 나면 오류를 출력한다.
 
-        in = open(input, O_RDONLY);
-        out = open(output, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+	out = open(fname_out, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+	// fname_out을 쓰기로 열고, 생성옵션까지 추가하였다.
+	// 추가적으로 소유자에게 읽기와 쓰기 권한을 주도록 지정하였다.
+	// out은 fname_out 파일에 대한 파일 디스크립터다.
+	if ( out < 0 ) {
+		printf("Cannot Write the file\n");
+		exit(1);
+	} // 파일 쓰기에 오류가 나면 출력한다.
 
-        while((read = read(in, block, sizeof(block))) > 0) {
-         write(out, block, read);
-         printf(".");
-
-     }
-
-     close(in);
-     close(out);
-     printf("\ncopy end\n");
-     return 0;
-    }
+	// fname_in에서 fname_out으로 8byte씩 복사케 한다.
+	time(&time1);	
+	while (read(in, buf, sizeof(buf)) > 0) {
+		time(&time2); //time2 설정, 8바이트씩 파일을 복사할 때 마다
+		write(out, buf, sizeof(buf));
+		if ( (time2 - time1) % TIMEINTERVAL== 1 ) {
+			printf(".\n"); // 파일 복사 수행에  1초마다 한번씩 .찍기
+			sleep(1);
+		}
+	}
+	printf("%s -> %s \n", fname_in, fname_out);
+	printf("파일 복사 완료!!\n");
+	exit(0);	
+}
