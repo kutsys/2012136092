c315c1c528e0f97d635a2bede7319552e8b2d032
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-12-12 15:21:43 +0900

    Update mtest.c

--- Lab5/mtest.c
+++ Lab5/mtest.c
@@ -1,13 +1,8 @@
 #include"mtest.h"
-#include<stdio.h>
-#include<stdlib.h>
-extern int InFoo(), InBoo(), InBar();
+
 int main()
 {	
 	InFoo();
 	InBoo();
 	InBar();
-	printf("And this is Main Function.\n");
-	printf("Hello World!\n");
-	exit(0);
 }
